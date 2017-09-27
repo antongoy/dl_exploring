b442164efa9514df82c78ffad4f38fec56a40db2
Anton Goy <g.a.s.s@mail.ru>
2017-09-27 22:54:09 +0300

    Add train infrastructure

--- train_net.py
+++ train_net.py
@@ -1,13 +1,18 @@
 import argparse
 
 import torch
+import torchvision
 import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+from torchvision import transforms
 
 from inceptions import InceptionV1
 
 
 class MiniGoogLeNet(nn.Module):
     def __init__(self):
+        super(MiniGoogLeNet, self).__init__()
         self.relu = nn.ReLU(inplace=True)
         self.first_conv = nn.Conv2d(3, 32, kernel_size=7, stride=2, padding=3)
         self.inception1 = InceptionV1(32, 16, 16, 16, 8, 16, 16)
@@ -30,3 +35,47 @@ class MiniGoogLeNet(nn.Module):
         x = x.view(x.size(0), -1)
         x = self.linear(x)
         return x
+
+
+parser = argparse.ArgumentParser()
+
+print('==> Preparing data..')
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+
+net = MiniGoogLeNet()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9, weight_decay=5e-4)
+net.train(True)
+train_loss = 0
+total = 0
+correct = 0
+for batch_idx, (inputs, targets) in enumerate(trainloader):
+    optimizer.zero_grad()
+    inputs, targets = Variable(inputs), Variable(targets)
+    outputs = net(inputs)
+    loss = criterion(outputs, targets)
+    loss.backward()
+    optimizer.step()
+
+    train_loss += loss.data[0]
+    _, predicted = torch.max(outputs.data, 1)
+    total += targets.size(0)
+    correct += predicted.eq(targets.data).cpu().sum()
+
+    print('\rLoss: %.3f | Acc: %.3f%% (%d/%d)' % (train_loss / (batch_idx + 1), 100. * correct / total, correct, total))
